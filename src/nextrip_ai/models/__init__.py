1e9ae10a87fb93e0dd9db8173346aa2a3d245ed8
gdev <125574259+gisachris@users.noreply.github.com.>
2026-08-12 10:00:00 +0200

    feat: implement document manifest database model for CDC manifest tracking

---
+++ src/nextrip_ai/models/__init__.py
@@ -0,0 +1,4 @@
+from nextrip_ai.models.user import User
+from nextrip_ai.models.trip import Trip
+from nextrip_ai.models.itinerary import Itinerary
+from nextrip_ai.models.document_manifest import DocumentManifest
